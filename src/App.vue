be6fbcf5f7cb2a0dc40a0d5150db066e7cfb3693
AleksandrSl <sl.aleksandr28@gmail.com>
2022-04-19 22:28:43 +0300

    kek

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div>kek
 </template>
 
 <script>
